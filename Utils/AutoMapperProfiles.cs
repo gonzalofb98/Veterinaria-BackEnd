bf5258b495e3d6f4b4d5b52292d116cd24fcbb3b
Gonzalo <gonzalofb98@gmail.com>
2023-02-10 00:34:29 -0300

    Profile Mapper

---
+++ Utils/AutoMapperProfiles.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using Dtos.Dto;
+using Entities;
+
+namespace Utils
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<Cliente, ClienteDto>();
+            CreateMap<ClienteDto, Cliente>();
+            CreateMap<Pedido, PedidoDto>();
+            CreateMap<PedidoDto, Pedido>();
+            CreateMap<ComboDto, Combo<Mascota>>();
+            CreateMap<Combo<Mascota>, ComboDto>();
+            CreateMap<Mascota, MascotaDto>();
+            CreateMap<MascotaDto, Mascota>();
+            CreateMap<Vendedor, VendedorDto>();
+            CreateMap<VendedorDto, Vendedor>();
+        }
+    }
+}
